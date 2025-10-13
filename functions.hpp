84a4dcc50d19209f2bf0015d647a9a46bd1c2237
fatima068 <wasimfatima4@gmail.com>
2025-10-13 20:21:50 +0500

    added stop word removal feature

--- functions.hpp
+++ functions.hpp
@@ -9,6 +9,12 @@ using namespace std;
 #include <filesystem>
 namespace fs = std::filesystem;
 
+const string stopWords[] = {
+    "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "does", "did", "doing", "a", "an", "the", "and", "but", "because", "as", "until", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"
+};
+
+const int numStopWords = sizeof(stopWords) / sizeof(stopWords[0]);
+
 // read line from txt, clean it using removePunctuation(), then return that line and parse that
 string removePunctuation(const string& line) {
     string cleaned;
@@ -20,10 +26,20 @@ string removePunctuation(const string& line) {
     return cleaned;
 } // if the file has stuff like don’t or it’s, we want to keep the apostrophe but this function isnt doing that, consider it if time left end mei 
 
+bool isStopWord(const string& word) {
+    for (int i = 0; i < numStopWords; i++) {
+        if (word == stopWords[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
 void tokenize(const string& line, safeArray<string>& tokenArr){
     stringstream ss(line);
     string token;
     while (ss >> token) {
+        if(isStopWord(token) == false)
         tokenArr.pushback(token);
     }
 }
